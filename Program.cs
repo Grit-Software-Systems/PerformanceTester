97a814217efa6b4111c86a79c92946b9d8d851ab
Clifford Wong <cliffw2007@hotmail.com>
2022-04-21 14:28:28 -0700

    First checkin

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfRunner
+{
+    class Program
+    {
+        static int threadCount = int.Parse(Config("threads", "1"));
+        static int iterations = int.Parse(Config("iterations", "1"));
+
+        static string suiteName = "";
+        static string appInsightsInstrumentationkey = "";
+        static string testRunnerPath = "";
+
+
+        static void Main(string[] args)
+        {
+            ParseArgs(args);
+
+            TestCases testCases = new TestCases();
+
+            string[] suites = suiteName.Split(',');
+
+            for (int iter = 0; iter < iterations; iter++)
+            {
+                Parallel.ForEach(suites,
+                    new ParallelOptions { MaxDegreeOfParallelism = threadCount },
+                    suite => 
+                {
+                    testCases.Execute(
+                        testRunnerPath,
+                        suite,
+                        appInsightsInstrumentationkey);
+                });
+            }
+        }
+
+
+        static void ParseArgs(string[] args)
+        {
+            ProcessArg(args, "suiteNamePrefix", ref suiteName);
+            ProcessArg(args, "executablePathPrefix", ref testRunnerPath);
+            ProcessArg(args, "appInsightsInstrumentationKeyPrefix", ref appInsightsInstrumentationkey);
+
+            if(!testRunnerPath.Contains(".exe"))
+            {
+                testRunnerPath = Directory.GetFiles(testRunnerPath, "b2ctestcaserunner.exe").FirstOrDefault();
+            }
+
+            string value = "";
+            ProcessArg(args, "threadsPrefix", ref value);
+            if (!string.IsNullOrEmpty(value)) 
+                threadCount = int.Parse(value);    
+
+            ProcessArg(args, "iterationsPrefix", ref value);
+            if (!string.IsNullOrEmpty(value))
+                iterations = int.Parse(value);
+        }
+
+
+        static void ProcessArg(string[] args, string prefixName, ref string field)
+        {
+            string prefix = Config(prefixName);
+            string value = args.Where(a => a.ToLower().Contains(prefix.ToLower())).FirstOrDefault();
+            if(!string.IsNullOrEmpty(value))
+            {
+                field = value.Substring(prefix.Length);
+            }
+        }
+
+        static string Config(string key, string defaultValue = "")
+        {
+            string value = ConfigurationManager.AppSettings[key];
+            return string.IsNullOrEmpty(value) ? defaultValue : value;
+        }
+    }
+}
---
+++ TestCases.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PerfRunner
+{
+    class TestCases
+    {
+        public void Execute(
+            string exePath,
+            string suiteName,
+            string appInsightsInstrumentationKey = "")
+        {
+            Dictionary<string, string> env = new Dictionary<string, string>();
+            env.Add("appInsightsInstrumentationKey", appInsightsInstrumentationKey);
+
+            string jsonSuite = suiteName;
+
+            StartProcess(exePath, $"{suiteName}", env);
+        }
+
+
+        void StartProcess(string executable, string arguments = "", Dictionary<string, string> env = null, string workingDirectory = "")
+        {
+            ProcessStartInfo proc = new System.Diagnostics.ProcessStartInfo();
+            proc.FileName = @"C:\windows\system32\cmd.exe";
+            proc.UseShellExecute = false;
+            if (!string.IsNullOrEmpty(workingDirectory))
+            {
+                proc.WorkingDirectory = workingDirectory;
+            }
+            else
+            {
+                string dir = Path.GetDirectoryName(executable);
+                proc.WorkingDirectory = Path.GetDirectoryName(executable);
+            }
+            proc.Arguments = $"/c {executable} {arguments}";
+
+            foreach (string key in env.Keys)
+            {
+                proc.Environment.Add(key, env[key]);
+            }
+
+            Process p = Process.Start(proc);
+            p.WaitForExit();
+        }
+
+    }
+}
